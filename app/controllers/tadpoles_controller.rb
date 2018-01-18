eedca1a06cbdd0f03858de1bb3c9ee97fc4cd086
Justin Tooley <jstooley823@gmail.com>
2018-01-18 04:08:35 +0000

    automatically backed up by learn

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -2,13 +2,14 @@ class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphose]
 
   def metamorphose
-   @frog = Frog.new(name: @tadpole.name, color: @tadpole.color, pond: @tadpole.pond)
-   if @frog.save
-     @tadpole.destroy
-     redirect_to @frog
-   else
-     render :show
-   end
+    @frog = Frog.new(name: @tadpole.name, color: @tadpole.color, pond: @tadpole.pond)
+    if @frog.save
+      @tadpole.destroy
+      redirect_to @frog
+    else
+      render :show
+    end
+      
   end
 
   def index
